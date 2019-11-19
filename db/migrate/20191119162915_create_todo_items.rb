1d6f0f5e2fbd02b4990047ec26ced6bbb2f2b407
Steve Polito <info@stevepolitodesign.com>
2019-11-19 11:31:30 -0500

    set default value to false on complete column

--- db/migrate/20191119162915_create_todo_items.rb
+++ db/migrate/20191119162915_create_todo_items.rb
@@ -3,7 +3,7 @@ class CreateTodoItems < ActiveRecord::Migration[6.0]
     create_table :todo_items do |t|
       t.string :title
       t.references :user, null: false, foreign_key: true
-      t.boolean :complete
+      t.boolean :complete, default: false
 
       t.timestamps
     end
