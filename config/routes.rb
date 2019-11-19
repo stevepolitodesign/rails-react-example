791c27c518deab28644139dee8ed77ebf002c403
Steve Polito <info@stevepolitodesign.com>
2019-11-19 11:57:11 -0500

    create namespaced api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  namespace :api do
+    namespace :v1 do
+      resources :todo_items
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
