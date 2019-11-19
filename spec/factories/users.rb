74dfb4a21ff8ab773d9a993e868e1d8603c3dd13
Steve Polito <info@stevepolitodesign.com>
2019-11-19 11:24:16 -0500

    write user model specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :user do
-    
+    sequence(:email) { |n| "user-#{n}@example.com" }
+    password { "password" }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe "creation" do
+    let!(:user) { FactoryBot.create(:user) }
+    it "can be created" do
+      expect(user).to be_valid
+    end
+  end
+
+  describe "validations" do
+    let(:user) { FactoryBot.build(:user) }
+    let(:duplicate_user) { FactoryBot.build(:user) }
+    it "must have an email address" do
+      user.email = nil
+      expect(user).to_not be_valid
+    end
+    it "must have a unique email address" do
+      user.save!
+      duplicate_user.email = user.email
+      expect(duplicate_user).to_not be_valid
+    end
+    it "must have a password" do
+      user.password = nil
+      expect(user).to_not be_valid
+    end        
+  end
+
 end
