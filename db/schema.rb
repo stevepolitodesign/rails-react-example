28babec6ced696e3d04d5d3e6b4404273de016cc
Steve Polito <info@stevepolitodesign.com>
2019-11-19 13:58:14 -0500

    rollback post migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_19_170500) do
+ActiveRecord::Schema.define(version: 2019_11_19_162915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
   create_table "todo_items", force: :cascade do |t|
     t.string "title"
