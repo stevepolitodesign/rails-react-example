59b43104d9b5d262568ff498ff78c1ff15124b5f
Steve Polito <info@stevepolitodesign.com>
2019-11-19 13:57:48 -0500

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+2.times do |i|
+    User.create(email: "user-#{i}.example.com", password: "password")
+end
+
+User.all.each_with_index do |u, i|
+    u.todo_items.create(title: "To Do Item #{i} for #{u.email}", complete: i % 2 ? true : false  )
+end
