7f05144008d211ae102539359a4938745eadd344
Steve Polito <info@stevepolitodesign.com>
2019-11-19 13:58:50 -0500

    remove posts resource and set default format to json for todo_items resource.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  resources :posts
   devise_for :users
   namespace :api do
     namespace :v1 do
-      resources :todo_items
+      resources :todo_items, defaults: { format: :json }
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
