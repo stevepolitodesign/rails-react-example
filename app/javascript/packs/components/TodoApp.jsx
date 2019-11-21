93b5fee7744ce7b18e5dcd09ab978fd0960432fa
Steve Polito <info@stevepolitodesign.com>
2019-11-21 13:53:00 -0500

    add state to ToDoForm component

--- app/javascript/packs/components/TodoApp.jsx
+++ app/javascript/packs/components/TodoApp.jsx
@@ -12,7 +12,6 @@ class TodoApp extends React.Component {
         todo_items: [],
         current_user: {}
       };
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
@@ -40,34 +39,10 @@ class TodoApp extends React.Component {
     }
   }
 
-  handleSubmit(e) {
-    e.preventDefault();
-    this.setAxiosHeaders();
-    axios.post('/api/v1/todo_items', {
-      todo_item: {
-        title: 'a hard coded to do item',
-        complete: false,
-        user_id: 1
-      }
-    })
-    .then( (response) => {
-      console.log(response);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-  }
-
-  setAxiosHeaders() {
-    // https://medium.com/@zayneabraham/ruby-on-rails-csrf-protection-with-react-js-65dd84b8edad
-    const csrfToken = document.querySelector('[name=csrf-token]').content
-    axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken
-  }
-
   render() {
     return (
       <>
-        <TodoForm handleSubmit={this.handleSubmit} />
+        <TodoForm />
         { this.state.todo_items.map( todo_item => <TodoItem key={todo_item.id} todo_item={todo_item}/>) }
       </>
     );
--- app/javascript/packs/components/TodoForm.jsx
+++ app/javascript/packs/components/TodoForm.jsx
@@ -1,17 +1,43 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+class TodoForm extends React.Component {
+    constructor(props){
+        super(props)
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleSubmit(e) {
+        e.preventDefault();
+        console.log(e);
+        // this.setAxiosHeaders();
+        // axios.post('/api/v1/todo_items', {
+        //   todo_item: {
+        //     title: 'a hard coded to do item',
+        //     complete: false,
+        //     user_id: 1
+        //   }
+        // })
+        // .then( (response) => {
+        //   console.log(response);
+        // })
+        // .catch((error) => {
+        //   console.log(error);
+        // });
+    }
 
-const TodoForm = (props) => {
-    return(
-        <form onSubmit={props.handleSubmit}>
-            <input type="text" name="title" required />
-            <button>Add To Do Item</button>
-        </form>
-    )
+    setAxiosHeaders() {
+        // https://medium.com/@zayneabraham/ruby-on-rails-csrf-protection-with-react-js-65dd84b8edad
+        const csrfToken = document.querySelector('[name=csrf-token]').content
+        axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken
+      }
+
+    render() {
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" name="title" required />
+                <button>Add To Do Item</button>
+            </form>
+        )    
+    }
+
 }
 
 export default TodoForm
-
-TodoForm.propTypes = {
-    handleSubmit: PropTypes.func.isRequired
-};
