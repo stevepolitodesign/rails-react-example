fe326fb574c3be36e8e8f2ea6501c10e78fb76f9
Steve Polito <info@stevepolitodesign.com>
2019-12-03 11:41:35 -0500

    create shared setAxiosHeaders component

---
+++ app/javascript/packs/components/AxiosHeaders.jsx
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+const setAxiosHeaders = () => {
+    const csrfToken = document.querySelector('[name=csrf-token]').content
+    axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken
+}
+
+export default setAxiosHeaders;
+
--- app/javascript/packs/components/TodoForm.jsx
+++ app/javascript/packs/components/TodoForm.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import axios from 'axios'
+import setAxiosHeaders from './AxiosHeaders'
 class TodoForm extends React.Component {
 
     constructor(props){
@@ -13,7 +14,7 @@ class TodoForm extends React.Component {
 
     handleSubmit(e) {
         e.preventDefault();
-        this.setAxiosHeaders();
+        setAxiosHeaders();
         axios.post('/api/v1/todo_items', {
           todo_item: {
             title: this.titleRef.current.value,
@@ -29,11 +30,6 @@ class TodoForm extends React.Component {
           console.log(error);
         });
         e.target.reset();
-    }
-
-    setAxiosHeaders() {
-        const csrfToken = document.querySelector('[name=csrf-token]').content
-        axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken
     }
 
     render() {
