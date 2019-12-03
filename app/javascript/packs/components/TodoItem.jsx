68a948d4577a5ab7b995ded21abd9639791d4656
Steve Polito <info@stevepolitodesign.com>
2019-12-03 12:06:22 -0500

    hardcode put request

--- app/javascript/packs/components/TodoItem.jsx
+++ app/javascript/packs/components/TodoItem.jsx
@@ -1,23 +1,41 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import axios from 'axios'
+import setAxiosHeaders from './AxiosHeaders'
+
 class TodoItem extends React.Component {
     constructor(props) {
         super(props)
+        this.todo_item = this.props.todo_item
         this.handleChange = this.handleChange.bind(this);
         this.inputRef = React.createRef();
+        this.completedRef = React.createRef();
     }
     handleChange(e) {
-        console.log(e);
+        setAxiosHeaders();
+        axios.put(`/api/v1/todo_items/${this.todo_item.id}`, {
+          todo_item: {
+            title: 'UPDATED TITLE',
+            complete: true
+          }
+        })
+        .then( (response) => {
+            console.log(response);
+        })
+        .catch((error) => {
+          // TODO handle this
+          console.log(error);
+        });        
     }
     render(){
-        const {id, title, complete } = this.props.todo_item
+        const { id, title, complete } = this.todo_item;
         return (
             <div index={id}>
-                <input type="text" value={title} ref={this.inputRef} onChange={this.handleChange} />
+                <input type="text" value={title} onChange={this.handleChange} ref={this.inputRef} />
                 <label>
                     Complete:
-                    <input type="boolean" checked={complete} type="checkbox" />
+                    <input type="boolean" checked={complete} type="checkbox" onChange={this.handleChange} ref={this.completedRef} />
                 </label>
             </div>
         )   
