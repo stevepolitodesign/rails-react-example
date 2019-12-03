3ebb207e9f85c16cd51aa826f6a4a3311727bfec
Steve Polito <info@stevepolitodesign.com>
2019-12-03 11:37:10 -0500

    convert TodoItem into a class component

--- app/javascript/packs/components/TodoItem.jsx
+++ app/javascript/packs/components/TodoItem.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const TodoItem = (props) => {
-    const {id, title, complete } = props.todo_item
-    return (
-        <div index={id}>
-            <input type="text" value={title} />
-            <label>
-                Complete:
-                <input type="boolean" checked={complete} type="checkbox" />
-            </label>
-        </div>
-    )
+class TodoItem extends React.Component {
+    constructor(props) {
+        super(props)
+        this.handleChange = this.handleChange.bind(this);
+        this.inputRef = React.createRef();
+    }
+    handleChange(e) {
+        console.log(e);
+    }
+    render(){
+        const {id, title, complete } = this.props.todo_item
+        return (
+            <div index={id}>
+                <input type="text" value={title} ref={this.inputRef} onChange={this.handleChange} />
+                <label>
+                    Complete:
+                    <input type="boolean" checked={complete} type="checkbox" />
+                </label>
+            </div>
+        )   
+    }
 }
 
 export default TodoItem
