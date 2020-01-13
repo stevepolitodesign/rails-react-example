bf7cbd0bd00b3598ae7496e71e71d7a3c09de7d4
Steve Polito <info@stevepolitodesign.com>
2020-01-13 07:35:52 -0500

    make todo feature specs require js

--- spec/features/todos_flow_spec.rb
+++ spec/features/todos_flow_spec.rb
@@ -1,39 +1,39 @@
 require 'rails_helper'
 
 RSpec.feature "TodosFlows", type: :feature do
-  describe "creating a todo item" do
+  describe "creating a todo item", js: true do
     context "todo item is valid" do
       it "creates a new todo item on the top of the list" do
         skip
       end
     end
-    context "todo item is not valid" do
+    context "todo item is not valid", js: true do
       it "displays an error message" do
         skip
       end
     end
   end
 
-  describe "updating a todo item" do
+  describe "updating a todo item", js: true do
     context "todo item is valid" do
       it "updates the todo item" do
         skip
       end
     end
-    context "todo item is valid" do
+    context "todo item is valid", js: true do
       it "displays an error message" do
         skip
       end
     end    
   end
 
-  describe "deleting a todo item" do
+  describe "deleting a todo item", js: true do
     it "removes the todo item from the list" do
       skip
     end
   end
 
-  describe "filtering todo items" do
+  describe "filtering todo items", js: true do
     it "only shows incomplete todo items" do
       skip
     end
