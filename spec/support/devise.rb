a14fd7087ed715ec25042ccb3e275f62f2c6cb1c
Steve Polito <info@stevepolitodesign.com>
2020-01-13 07:45:07 -0500

    add support for devise helpers

--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -2,4 +2,8 @@ RSpec.configure do |config|
     config.include Devise::Test::ControllerHelpers, type: :controller
     config.include Devise::Test::ControllerHelpers, type: :view
     config.include Devise::Test::IntegrationHelpers, type: :feature
+    config.include Warden::Test::Helpers
+    config.after :each do
+        Warden.test_reset!
+    end
 end
