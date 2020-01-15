71c9dc98802b725fbe3d5a89402c915475eb75a1
Steve Polito <info@stevepolitodesign.com>
2020-01-15 08:05:59 -0500

    write failing update test

--- spec/features/todos_flow_spec.rb
+++ spec/features/todos_flow_spec.rb
@@ -5,21 +5,29 @@ RSpec.feature "TodosFlows", type: :feature do
     let(:user) { FactoryBot.create(:user) }
     valid_todo_item = 'this is a new note'
     in_valid_todo_item = ''
-      it "creates a new todo item on the top of the list" do
-        login_as(user, :scope => :user)
-        visit root_path
-        fill_in('title', with: valid_todo_item)
-        click_button('Add To Do Item')
-        new_todo_item = find('.table > tbody > tr:first-of-type td:nth-child(2) input:first-of-type')
-        expect(new_todo_item.value).to eq(valid_todo_item)
-      end
+    it "creates a new todo item on the top of the list" do
+      login_as(user, :scope => :user)
+      visit root_path
+      fill_in('title', with: valid_todo_item)
+      click_button('Add To Do Item')
+      new_todo_item = find('.table > tbody > tr:first-of-type td:nth-child(2) input:first-of-type')
+      expect(new_todo_item.value).to eq(valid_todo_item)
     end
   end
 
   describe "updating a todo item", js: true do
+    let(:user_with_todo_items) { FactoryBot.create(:user_with_todo_items) }
+    updated_todo_item_text = 'updated todo'
     context "todo item is valid" do
       it "updates the todo item" do
-        skip
+        login_as(user_with_todo_items, :scope => :user)
+        visit root_path
+        todo_item = user_with_todo_items.todo_items.first
+        fill_in("todoItem__title-#{todo_item.id}", with: updated_todo_item_text)
+        binding.pry
+        visit root_path
+        updated_todo_item = find('.table > tbody > tr:first-of-type td:nth-child(2) input:first-of-type')
+        expect(updated_todo_item.value).to eq(updated_todo_item_text)
       end
     end
     context "todo item is valid", js: true do
