b0c26b56dcbd8d85d8491a1505bcd2b461cfaf85
Steve Polito <info@stevepolitodesign.com>
2020-01-15 07:26:35 -0500

    remove spec testing against empty todo items

--- spec/features/todos_flow_spec.rb
+++ spec/features/todos_flow_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature "TodosFlows", type: :feature do
   describe "creating a todo item", js: true do
     let(:user) { FactoryBot.create(:user) }
     valid_todo_item = 'this is a new note'
-    context "todo item is valid" do
+    in_valid_todo_item = ''
       it "creates a new todo item on the top of the list" do
         login_as(user, :scope => :user)
         visit root_path
@@ -12,11 +12,6 @@ RSpec.feature "TodosFlows", type: :feature do
         click_button('Add To Do Item')
         new_todo_item = find('.table > tbody > tr:first-of-type td:nth-child(2) input:first-of-type')
         expect(new_todo_item.value).to eq(valid_todo_item)
-      end
-    end
-    context "todo item is not valid", js: true do
-      it "displays an error message" do
-        skip
       end
     end
   end
