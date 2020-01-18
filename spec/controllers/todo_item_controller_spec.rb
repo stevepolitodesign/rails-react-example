31ab2c7ff4adb8444662214bbd30ea564e536163
Steve Polito <info@stevepolitodesign.com>
2020-01-18 17:07:41 -0500

    write pending todo_item controller specs

--- spec/controllers/todo_item_controller_spec.rb
+++ spec/controllers/todo_item_controller_spec.rb
@@ -1,5 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe TodoItemController, type: :controller do
+    describe "index" do
+        it "displays the current users todo items" do
+            skip
+        end
+    end
+
+    describe "show" do
+        context "when authenticated" do
+            it "returns a todo_item" do
+                skip
+            end
+        end
+        context "when not authenticated" do
+            it "returns unauthorized" do
+                skip
+            end
+        end        
+    end
+
+    describe "create" do
+        context "when authenticated" do
+            it "returns a todo_item" do
+                skip
+            end
+        end
+        context "when not authenticated" do
+            it "returns unauthorized" do
+                skip
+            end
+        end        
+    end
+    
+    describe "update" do
+        context "when authenticated" do
+            it "returns a todo_item" do
+                skip
+            end
+        end
+        context "when not authenticated" do
+            it "returns unauthorized" do
+                skip
+            end
+        end        
+    end
+    
+    describe "destroy" do
+        it "returns no content" do
+            skip
+        end      
+    end
 
 end
